7cf8566cc53fba001a9bc80e9474d5fd5b003712
SAEZ1205 <saez123m@gmail.com>
2026-08-29 23:32:50 -0500

    Fix Android lifecycle visibility for Capacitor 8

--- android-src/MainActivity.java
+++ android-src/MainActivity.java
@@ -111,7 +111,7 @@ public class MainActivity extends BridgeActivity {
     }
 
     @Override
-    protected void onDestroy() {
+    public void onDestroy() {
         if (tts != null) { tts.stop(); tts.shutdown(); }
         super.onDestroy();
     }
